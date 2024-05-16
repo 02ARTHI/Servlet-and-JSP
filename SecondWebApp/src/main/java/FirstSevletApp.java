a826dbbdd95672fcea7f9060353860024ff66330
Arthi Selavarj <164236172+02ARTHI@users.noreply.github.com>
2024-05-16 22:31:07 +0530

    dynamic web app

---
+++ SecondWebApp/src/main/java/FirstSevletApp.java
@@ -0,0 +1,53 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstSevletApp
+ */
+@WebServlet("/firstServlet")
+public class FirstSevletApp extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstSevletApp() {
+        super();
+        System.out.println("servlet object is created");
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("uname");
+		String city = request.getParameter("ucity");
+		
+		PrintWriter writer =response.getWriter();
+//		writer.println("Hello " + name);
+//		writer.println("I know you're from " + city);
+		writer.println("<html> <head> <title> Second App</title></head>");
+		writer.println("<body bgcolor='cyan'> <h1><marquee> Welcome to our dynamic app </marquee></h1>");
+		writer.println("<table>");
+		writer.println("<tr> <th>NAME</th>  <th>CITY</th> </tr>");
+		writer.println("<tr><td> "+name+"</td> <td> "+city+"</td> </tr>");
+		
+		writer.println("</table></body>");
+		
+		writer.println("</html>");
+		
+		writer.close();
+		
+	}
+
+}
